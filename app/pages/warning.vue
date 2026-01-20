c78b02432561239a6d0182c1c6c494c7f2cbc477
waliu idowu <120091240+waliyullahi1@users.noreply.github.com>
2026-01-20 14:25:10 +0100

    update

--- app/pages/warning.vue
+++ app/pages/warning.vue
@@ -25,7 +25,7 @@
       <!-- Student List -->
 <div class="grid grid-cols-1 items-center justify-center sm:grid-cols-2 gap-4">
         <div
-          v-for="(student, index) in students"
+          v-for="(student, index) in participants"
           :key="index"
           class="border flex  flex-col items-center justify-center rounded-lg p-4  gap-4"
         >
@@ -64,72 +64,106 @@
 
 <script setup>
 
- const  students= [
+
+
+      const  participants= [
         {
-          name: "Abdulwaheed Waliyullahi Idowu",
+          name: "Abdulwaheed Waliyullahi",
           matric: "24/25OY011",
           photo: "/images/waliu.jpg",
+          paid:false
         },
         {
-          name: "Dosunmu  Boluwatife Idowu",
+          name: "Dosunmu  Boluwatife",
           matric: "24/25OY038",
           photo: "/images/38.png",
+          paid:true
         },
          {
           name: "Mukadam  Badmos Imam",
           matric: "24/25OY057",
           photo: "/images/157.jpg",
+          paid:false
         },
         {
           name: "Nurudeen  Mujeebah Opeyemi",
           matric: "24/25OY059",
           photo: "/images/59.jpg",
+          paid:true
         },
       
         {
           name: "Otitoju Eunice Mercy",
           matric: "24/25OY072",
           photo: "/images/172.jpg",
+          paid:true
         },
         {
           name: "Oyerinde Deborah ",
           matric: "24/25OY073",
           photo: "/images/173.jpg",
+          paid:true
         },
                 {
           name: "Okanlawon Fayidat Oyindamola ",
           matric: "22/25OY073",
           photo: "/images/1fayidah.jpg",
+          paid:true
+        },
+         {
+          name: "Soliu Nusirah Adeoye",
+          matric: "24/25OY078",
+          photo: "/images/78.jpg",
+          paid:true
         },
           {
           name: "Sulyman suliyah oluwakemi ",
           matric: "24/25OY079",
           photo: "/images/79.jpg",
+          paid:true
+        },
+         {
+          name: "Taiwo abiodun Emmanuel ",
+          matric: "24/25OY080",
+          photo: "/images/80.jpg",
+          paid:true
+        },
+                  {
+          name: "Taofeeq Aishat Ttilope ",
+          matric: "24/25OY081",
+          photo: "/images/81.jpg",
+          paid:true
         },
         {
           name: "Umar Fatimat Adeshewa ",
           matric: "24/25OY082",
           photo: "/images/82.jpg",
+          paid:true
         },
+
         {
-          name: "Unachukwu Ifeoluma Precious ",
+          name: "Unachukwu Ifeoma Precious ",
           matric: "24/25OY083",
           photo: "/images/83.jpg",
+          paid:true
         },
-                {
+                {                                                                                                                                                                                                                          
           name: "Usman Aminat Bisola ",
           matric: "24/25OY084",
           photo: "/images/184.jpg",
+          paid:true
         },
          {
-          name: "Usman Aminat Bisola ",
+          name: "Yusuf Haneefat Adeshewa ",
           matric: "24/25OY085",
           photo: "/images/85.jpg",
+          paid:true
         },
         {
-          name: "Usman Aminat Bisola ",
+          name: "Yusuf Rukayat Morenikeji  ",
           matric: "24/25OY086",
           photo: "/images/86.jpg",
+          paid:true
         },
         
 
